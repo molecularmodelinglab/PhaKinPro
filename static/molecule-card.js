a7944952d36ae584fbf9408ed8f9e2deed786dfd
James Wellnitz <wellnitz.james@gmail.com>
2022-09-24 14:49:52 -0400

    added some loading gifs

--- static/molecule-card.js
+++ static/molecule-card.js
@@ -2,48 +2,28 @@ const moleculeWrapper = document.querySelector('.molecule-wrapper');
 const moleculeProperties = document.querySelector('.molecule-properties');
 const moleculeSVG = document.getElementById('molecule-svg');
 const moleculeSMILES = document.getElementById('molecule_smile_string');
-
+const loadingWrapper = document.querySelector('.loading-wrapper');
 
 export function showMoleculeWrapper() {
     if (moleculeWrapper.className.includes('hidden')) {
         moleculeWrapper.classList.remove('hidden');
     }
 }
+
 export function hideMoleculeWrapper() {
     if (!moleculeWrapper.className.includes('hidden')) {
         moleculeWrapper.classList.add('hidden');
     }
 }
 
-
-// export function displayMoleculeCard(moleculeData) {
-//     showMoleculeWrapper();
-//
-//     moleculeProperties.innerHTML = '';
-//     let repeatDiv = document.getElementById("dummy-preds");
-//     moleculeSMILES.innerHTML = moleculeData.SMILES;
-//     moleculeSVG.innerHTML = moleculeData.svg;
-//
-//     for (const [modelName, classification, confidence, ad, prob_svg, color] of moleculeData.pred_data) {
-//
-//         for(let i = 0; i < value; i++) {
-//         // create new model pred element block
-//         let newDiv = document.createElement("div");
-//         newDiv.id = "repeat" + new Date().getTime();
-//
-//         // fill that block with the relevant data
-//         let element = document.createElement('span');
-//         element.class = 'molecular-property';
-//         element.innerHTML = `${modelName} <p style="color:${color};">${classification}</p><p>Confidence: ${confidence}</p><p>Applicability Domain: ${ad}</p><p><svg>${prob_svg}</svg></p>`;
-//         newDiv.append(element);
-//
-//         // place it after the previous block
-//         repeatDiv.parentNode.insertBefore(newDiv, repeatDiv.nextSibling); //essentially an insertAfter
-//         }
-//     }
-// }
+export function hideLoadingWrapper() {
+    if (!loadingWrapper.className.includes('hidden')) {
+        loadingWrapper.classList.add('hidden');
+    }
+}
 
 export function displayMoleculeCard(moleculeData) {
+    hideLoadingWrapper();
     showMoleculeWrapper();
 
     moleculeProperties.innerHTML = '';
--- static/single-smiles.js
+++ static/single-smiles.js
@@ -4,17 +4,28 @@ import { clearErrorMessage, displayError } from "./error.js";
 
 const form = document.getElementById('single-smiles-form');
 const smilesInput = document.getElementById('smiles-input');
+const loadingWrapper = document.querySelector('.loading-wrapper');
 
 function resetElements() {
     hideMoleculeWrapper();
     clearErrorMessage();
 }
 
+export function showLoadingWrapper() {
+    if (loadingWrapper.className.includes('hidden')) {
+        loadingWrapper.classList.remove('hidden');
+    }
+}
+
 form.onsubmit = (event) => {
     event.preventDefault();
 
     resetElements();
-    
+
+    showLoadingWrapper();
+
+    loadingWrapper.scrollIntoView({behavior:"smooth"});
+
     fetch('/smiles', {
         method: 'POST',
         body: JSON.stringify({'smiles': smilesInput.value, 'options': getOptions()}),
